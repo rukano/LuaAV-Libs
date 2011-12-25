8f8693f04bdf0f764cfee56b2834c7c6a095e37c
Juan A. Romero <rukano@gmail.com>
2011-12-25 15:03:25 +0100

    example file updated showing a model and initGL

--- example.lua
+++ example.lua
@@ -4,18 +4,42 @@ local GL = gl
 require("lib.Math")
 require("lib.Graphics")
 require("lib.Patterns")
-win = require("lib.Window")
+win = require("lib.Window") -- easy window test
+Model = require("lib.Model")
 
-win.autoclear = true
+win.autoclear = false
+
+initGL("alpha")
+
+local monkOBJ = Model.readfile("monkey.raw")
+local monkRAW = Model.readfile("monkey.obj")
 
 seq = Pseq{1,2,3,4}
 for i=1, 20 do print(seq:next()) end
 
 function win:draw ()
-   setColorHSL(wrap(now()/3), 0.5, 0.5, 1)
+   -- draw tests
+   setColorHSL(wrap(now()/3), 0.5, 0.5, 0.3)
    rectangle("fill", -1 + sin(now()*TAU)*0.1, -1 + cos(now()*TAU)*0.1, 1.9, 1.9)
-   setColorHSL(0.3, 0.5, 0.5, 1)
+   setColorHSL(0.3, 0.5, 0.5, 0.3)
    rectangle("line", -1, -1, 2, 2)
+
+   -- MDOEL TEST
+   gl.PushMatrix()
+   local s = 0.25
+   gl.Scale(s, s, s)
+   gl.Translate(0, 0, 1)
+   gl.Rotate(0 + math.sin(now())*5, 0, 3, 4)
+   for i,t in ipairs(monkOBJ) do
+      gl.Color(i/#monkOBJ, 0.5, 0.6, 0.3)
+      gl.Begin(GL.TRIANGLES)
+      for j,v in ipairs(t) do gl.Vertex(unpack(v)) end
+      gl.End()
+   end
+   gl.PopMatrix()
+
+
 end
+
 
 
